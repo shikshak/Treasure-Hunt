8743e1f68e38e0dc4a210745a5670570ac38293d
tifloch <floch.tifenn@gmail.com>
2015-12-11 18:01:57 +0100

    Create Menu.java

---
+++ Menu.java
@@ -0,0 +1,37 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.io.*;
+import java.awt.geom.*;
+import java.awt.image.*;
+
+public class Menu extends JFrame{
+
+/**Attributes*/
+/*Images*/
+Image Anonymous_run;
+Image Bunny_run;
+Image Chicken_run;
+Image Chipmunk_run;
+Image Cowboy_run;
+Image Fox_run;
+Image Girl_Future_run;
+Image Jocker_run;
+Image Cat_run;
+Image Manga_Girl_run;
+Image Ninja_run;
+Image Roadrunner_run;
+Image Sony_run;
+Image Squirel_run;
+Image SuperHero_Man_run;
+
+	public Menu{
+		Toolkit T=Toolkit.getDefaultToolkit();
+        Anonymous_run = T.getImage("anonymous_run.gif");
+
+        //Create double array with all the images of the avatars and their names associated with them
+	}
+	public static void main (String[] args){
+
+	}
+}
